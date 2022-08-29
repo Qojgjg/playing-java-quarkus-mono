1772233f123e7757a60f6b1313049772b4cfcfcf
UruguayanDev <sebascotti.uru@gmail.com>
2022-08-29 13:34:15 +0200

    keycloak without docker-compose

--- myapp/src/main/java/org/acme/boundary/ApiApplication.java
+++ myapp/src/main/java/org/acme/boundary/ApiApplication.java
@@ -3,11 +3,22 @@ package org.acme.boundary;
 import javax.ws.rs.core.Application;
 
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeIn;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
+
+@SecurityScheme(
+securitySchemeName = "JWT", 
+description = "JWT authentication with bearer token", type = SecuritySchemeType.HTTP, 
+in = SecuritySchemeIn.HEADER, 
+scheme = "bearer", 
+ bearerFormat = "Bearer [token]") 
 @OpenAPIDefinition(
     tags = {
             @Tag(name="widget", description="Widget operations."),
@@ -22,7 +33,8 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
             email = "techsupport@example.com"),
         license = @License(
             name = "Apache 2.0",
-            url = "https://www.apache.org/licenses/LICENSE-2.0.html"))
+            url = "https://www.apache.org/licenses/LICENSE-2.0.html")),
+            security = @SecurityRequirement(name = "JWT")
 )
 public class ApiApplication extends Application {
 }
