6334648d00699d38da724bf7b7821034f2591d45
UruguayanDev <sebascotti.uru@gmail.com>
2022-09-22 17:57:27 +0200

    v0 init

--- export_service/src/main/java/inveox/exportservice/web/TestExport.java
+++ export_service/src/main/java/inveox/exportservice/web/TestExport.java
@@ -4,25 +4,25 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import inveox.exportservice.application.ExportService;
 import inveox.exportservice.infrastructure.inbound.dlo.dto.DigitalLabOrderDto;
 import inveox.exportservice.infrastructure.inbound.patient.dto.PatientDto;
 
 
-@Path("/export/")
+@Path("/exportservice/")
 public class TestExport {
 
     @Inject
     ExportService exportService;
 
-    @Path("dlo/mock/{dlo_id}")
+    @Path("laborder/mock/{dlo_id}")
     @GET
     public DigitalLabOrderDto getDLO(@PathParam("dlo_id") String dlo_id) {
 
         DigitalLabOrderDto dlo = exportService.getDigitalLabOrder(dlo_id);
-
-        System.out.println(dlo);
 
         return dlo;
     }
@@ -33,18 +33,17 @@ public class TestExport {
 
         PatientDto patientDto = exportService.getPatient(pat_id);
 
-        System.out.println(patientDto);
-
         return patientDto;
     }
 
-    @Path("/map/{dlo_id}")
+    @Path("/message/laborder/{dlo_id}")
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
     public String Map(@PathParam("dlo_id") String dlo_id) {
 
-        exportService.mapDLOtoORM_01(dlo_id);
+        String response=exportService.mapDLOtoORM_01(dlo_id).toString();
 
-        return "Data lab order "+ dlo_id + " was mapped check export Folder";
+        return response;
     }
  
 
